e25f26bd24247f56fcd4c7d0ae61569af0707d74
Maycol <maycolteles@hotmail.com>
2022-11-21 20:16:58 -0300

    feat: Adding the player.

---
+++ app/src/players/player.py
@@ -0,0 +1,59 @@
+"""
+Module containing the "Player" class.
+"""
+
+from ursina import BoxCollider, Vec3, color, held_keys
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+from ..guns.gun import Gun
+
+
+class Player(FirstPersonController):
+    """
+    Class to represent a player.
+    """
+
+    def update(self) -> None:
+        """
+        Method to update the player.
+
+        This method is called on each frame.
+        """
+        super().update()
+
+        if held_keys['left mouse']:
+            self.gun.shoot()
+
+    def __init__(self) -> None:
+        """
+        Constructor to setup the Player.
+        """
+        self._set_player()
+        self._set_collider()
+        self._set_gun()
+
+    def _set_player(self) -> None:
+        """
+        Private Method to set the player entity.
+        """
+        player_attributes = {
+            "model": "cube",
+            "z": -10,
+            "color": color.orange,
+            "origin_y": -0.5,
+            "speed": 8
+        }
+
+        super().__init__(**player_attributes)
+
+    def _set_collider(self) -> None:
+        """
+        Private Method to set the player's collider.
+        """
+        self.collider = BoxCollider(self, Vec3(0, 1, 0), Vec3(1, 2, 1))
+
+    def _set_gun(self) -> None:
+        """
+        Private Method to set the player gun.
+        """
+        self.gun = Gun()
